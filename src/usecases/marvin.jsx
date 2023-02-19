bf6cf6ca155bfb52f53a6b524e28a951cae5282d
Jovan Krička <jovan@miro.com>
2023-02-19 22:40:04 +0100

    Implement initial version of Marvin with chat-only

--- src/usecases/marvin.jsx
+++ src/usecases/marvin.jsx
@@ -1,19 +1,58 @@
 import * as React from 'react';
 import {useState} from 'react';
+import {getAnswerFromChatGpt} from "../apis/openAiApi";
 
 const Marvin = () => {
 
     const [state, setState] = useState({
+        input: '',
         conversation: []
     });
 
     React.useEffect(() => {
     }, []);
 
+    const handleChangedInput = (event) => {
+        setState({
+            input: event.target.value,
+            conversation: state.conversation
+        })
+    };
+
+    const handleMarvinSend = async () => {
+        const chatGptAnswer = await getAnswerFromChatGpt(state.input)
+        const lastKey = state.conversation.length === 0 ? 0 : state.conversation[state.conversation.length - 1].key;
+        setState({
+            input: '',
+            conversation: state.conversation.concat([
+                {author: 'You', key: lastKey + 1, text: state.input},
+                {author: 'Marvin', key: lastKey + 2, text: chatGptAnswer}])
+        })
+    }
+
+    const renderChatHistory = () => {
+        const conversationCopy = structuredClone(state.conversation);
+        return <div id='marvinChatHistory' className='marvinChatHistory'>
+            {conversationCopy
+                .sort((a, b) => b.key - a.key)
+                .map(message => <p key={message.key}> <b>{message.author} ></b> {message.text}</p>)}
+        </div>
+    }
+
     return (
-        <div className="grid wrapper">
+        <div className="grid wrapper usecase">
             <div className="cs1 ce12">
                 <img src="/src/assets/marvin.png" alt=""/>
+            </div>
+            <div className="cs1 ce12">
+                <h1>Brainstorm through chatting with Marvin</h1>
+            </div>
+            <div className="cs1 ce12">
+                {renderChatHistory()}
+                <input className="input marvinInput" value={state.input} onChange={handleChangedInput}/>
+                <a className="button button-primary marvinSendButton" onClick={handleMarvinSend}>
+                    Send
+                </a>
             </div>
         </div>
     );
